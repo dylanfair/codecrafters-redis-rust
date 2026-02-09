12a64931abf480a550e88335d1f37beaf8ab5842
Dylan <carringtonfair.dylan@gmail.com>
2026-02-08 21:10:25 -0500

    Should now be able to handle incoming requests and send back errors

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use std::io::{Read, Write};
+use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 
@@ -27,39 +27,73 @@ fn main() -> Result<()> {
 }
 
 fn handle_stream(mut stream: TcpStream) {
+    let mut reader = BufReader::new(&stream);
     let mut stream_buf = String::new();
     let mut write_buf = String::new();
-    match stream.read_to_string(&mut stream_buf) {
+    match reader.read_line(&mut stream_buf) {
         Ok(nbytes) => {
             if nbytes == 0 {
                 return;
             }
-            println!("{}", &stream_buf);
+
+            // Check if first value is a *
+            if &stream_buf[0..1] != "*" {
+                return send_error(&mut stream, "First value is not *");
+            }
+            // Grab rest of lines based on first number
+            if let Ok(param_n) = &stream_buf[1..].trim().parse::<usize>() {
+                for _ in 0..param_n * 2 {
+                    match reader.read_line(&mut stream_buf) {
+                        Ok(_) => {}
+                        Err(e) => {
+                            return send_error(
+                                &mut stream,
+                                &format!("Failed to read lines that should be there: {}", e),
+                            );
+                        }
+                    }
+                }
+            } else {
+                return send_error(
+                    &mut stream,
+                    "Could not parse the number of parameters sent via protocol",
+                );
+            }
+
             match RedisProtocol::from_str(&stream_buf) {
                 Ok((_, redis_data)) => {
                     if !redis_data.valid() {
-                        eprintln!(
+                        return send_error(
+                            &mut stream,
                             "Data sent over doesn't match redis protocol - parameters length specified doesn't equal number of parameters sent",
                         );
                     }
                     handle_actions(redis_data, &mut write_buf);
                 }
                 Err(e) => {
-                    eprintln!("Failed to parse the incoming stream: {}", e);
+                    return send_error(
+                        &mut stream,
+                        &format!("Failed to parse the incoming stream: {}", e),
+                    );
                 }
             }
 
-            println!("{}", write_buf);
-            println!("{:?}", write_buf.as_bytes());
-            if let Err(e) = stream.write_all(write_buf.as_bytes()) {
-                eprintln!("Failed to write response: {}", e);
-            }
-            if let Err(e) = stream.flush() {
-                eprintln!("Failed to flush stream: {}", e);
-            }
+            send_response(&mut stream, &mut write_buf);
         }
-        Err(e) => {
-            eprintln!("Failed to read from stream: {}", e);
-        }
+        Err(e) => send_error(&mut stream, &format!("Failed to read from stream: {}", e)),
     }
+}
+
+fn send_response(stream: &mut TcpStream, write_buf: &mut String) {
+    if let Err(e) = stream.write_all(write_buf.as_bytes()) {
+        eprintln!("Failed to write response: {}", e);
+    }
+    if let Err(e) = stream.flush() {
+        eprintln!("Failed to flush stream: {}", e);
+    }
+}
+
+fn send_error(stream: &mut TcpStream, error: &str) {
+    eprintln!("{}", error);
+    send_response(stream, &mut format!("-ERR {}\r\n", error));
 }
