48b4fa511c013c4a9537fa7f6ebca8ba054bbf38
Dylan <carringtonfair.dylan@gmail.com>
2026-02-08 21:45:18 -0500

    corrected indexing...

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -2,7 +2,7 @@ use crate::protocol::parsing::RedisProtocol;
 
 pub fn handle_echo(protocol: RedisProtocol, write_buffer: &mut String) {
     // respond with $3\r\nhey\r\n
-    if let Some(to_be_echoed) = protocol.params_list.get(2) {
+    if let Some(to_be_echoed) = protocol.params_list.get(1) {
         let response = format!(
             "${}\r\n{}\r\n",
             to_be_echoed.param_size, to_be_echoed.param_value
--- src/protocol/mod.rs
+++ src/protocol/mod.rs
@@ -3,7 +3,7 @@ use crate::{commands::handle_echo, protocol::parsing::RedisProtocol};
 pub mod parsing;
 
 pub fn handle_commands(redis_data: RedisProtocol, write_buffer: &mut String) {
-    if let Some(action) = redis_data.params_list.get(1) {
+    if let Some(action) = redis_data.params_list.first() {
         match action.param_value.to_lowercase().as_str() {
             "ping" => write_buffer.push_str("+PONG\r\n"),
             "echo" => handle_echo(redis_data, write_buffer),
