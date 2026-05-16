07e1579d8b84690d379ba1d525012338dddcafc5
Dylan <carringtonfair.dylan@gmail.com>
2026-05-16 17:55:14 -0400

    now should have fixed the length

--- src/commands/info.rs
+++ src/commands/info.rs
@@ -5,11 +5,8 @@ pub fn handle_info(data: RedisProtocol, write_buffer: &mut String, server: &Arc<
     for i in 0..data.params_n {
         let value = data.params_list.get(i).expect("Within params_list range ");
         if value.param_value.to_uppercase() == "REPLICATION" {
-            write_buffer.push_str(&format!(
-                "${}\r\nrole:{}\r\n",
-                server.role.len(),
-                server.role
-            ));
+            let role = format!("role:{}", server.role);
+            write_buffer.push_str(&format!("${}\r\n{}\r\n", role.len(), role));
         }
     }
 }
