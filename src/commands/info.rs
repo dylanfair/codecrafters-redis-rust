7f7923680dc02939fa631dbcdafc2ee3bff94e14
Dylan <carringtonfair.dylan@gmail.com>
2026-05-17 23:15:33 -0400

    ok now we corrected bulk strings

--- src/commands/info.rs
+++ src/commands/info.rs
@@ -6,43 +6,31 @@ use std::sync::Arc;
 
 pub fn handle_info(data: RedisProtocol, write_buffer: &mut String, server: &Arc<RedisServer>) {
     let mut info_response = String::new();
-    let mut lines = 1;
 
     for i in 0..data.params_n {
         let value = data.params_list.get(i).expect("Within params_list range ");
         if value.param_value.to_uppercase() == "REPLICATION" {
             let role = format!("role:{}", server.role);
-            info_response.push_str(&format!("${}\r\n{}\r\n", role.len(), role));
+            info_response.push_str(&format!("{}", role));
             match server.role {
                 RedisRole::Master => {
-                    lines += 2;
-
                     let master_replid = format!(
-                        "master_replid:{}",
+                        "\r\nmaster_replid:{}",
                         server.master_repl.as_ref().unwrap().master_replid
                     );
                     let master_repl_offset = format!(
-                        "master_repl_offset:{}",
+                        "\r\nmaster_repl_offset:{}",
                         server.master_repl.as_ref().unwrap().master_repl_offset
                     );
 
-                    info_response.push_str(&format!(
-                        "${}\r\n{}\r\n",
-                        master_replid.len(),
-                        master_replid
-                    ));
-                    info_response.push_str(&format!(
-                        "${}\r\n{}\r\n",
-                        master_repl_offset.len(),
-                        master_repl_offset
-                    ));
+                    info_response.push_str(&master_replid);
+                    info_response.push_str(&master_repl_offset);
                 }
                 RedisRole::Slave => {}
             }
         }
     }
 
-    info_response = format!("*{lines}\r\n{info_response}");
-    println!("{info_response}");
+    info_response = format!("${}\r\n{}\r\n", info_response.len(), info_response);
     write_buffer.push_str(&info_response);
 }
