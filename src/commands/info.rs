17340fe748b1051461a2ec8277c36c34d047a16a
Dylan <carringtonfair.dylan@gmail.com>
2026-05-17 23:04:13 -0400

    think I've corrected bulk string response now

--- src/commands/info.rs
+++ src/commands/info.rs
@@ -5,13 +5,18 @@ use crate::{
 use std::sync::Arc;
 
 pub fn handle_info(data: RedisProtocol, write_buffer: &mut String, server: &Arc<RedisServer>) {
+    let mut info_response = String::new();
+    let mut lines = 1;
+
     for i in 0..data.params_n {
         let value = data.params_list.get(i).expect("Within params_list range ");
         if value.param_value.to_uppercase() == "REPLICATION" {
             let role = format!("role:{}", server.role);
-            write_buffer.push_str(&format!("${}\r\n{}\r\n", role.len(), role));
+            info_response.push_str(&format!("${}\r\n{}\r\n", role.len(), role));
             match server.role {
                 RedisRole::Master => {
+                    lines += 2;
+
                     let master_replid = format!(
                         "master_replid:{}",
                         server.master_repl.as_ref().unwrap().master_replid
@@ -20,12 +25,13 @@ pub fn handle_info(data: RedisProtocol, write_buffer: &mut String, server: &Arc<
                         "master_repl_offset:{}",
                         server.master_repl.as_ref().unwrap().master_repl_offset
                     );
-                    write_buffer.push_str(&format!(
+
+                    info_response.push_str(&format!(
                         "${}\r\n{}\r\n",
                         master_replid.len(),
                         master_replid
                     ));
-                    write_buffer.push_str(&format!(
+                    info_response.push_str(&format!(
                         "${}\r\n{}\r\n",
                         master_repl_offset.len(),
                         master_repl_offset
@@ -35,4 +41,8 @@ pub fn handle_info(data: RedisProtocol, write_buffer: &mut String, server: &Arc<
             }
         }
     }
+
+    info_response = format!("*{lines}\r\n{info_response}");
+    println!("{info_response}");
+    write_buffer.push_str(&info_response);
 }
