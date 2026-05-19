f08235c67de5d7c7f74420cd8dcd3e04f4501496
Dylan <carringtonfair.dylan@gmail.com>
2026-05-19 00:00:59 -0400

    fixed psync send

--- src/commands/psync.rs
+++ src/commands/psync.rs
@@ -3,7 +3,8 @@ use std::sync::Arc;
 use crate::{protocol::parsing::RedisProtocol, server::server::RedisServer};
 
 pub fn handle_psync(write_buffer: &mut String, server: &Arc<RedisServer>) {
-    write_buffer.push_str("+FULLRESYNC");
-    write_buffer.push_str(&server.master_repl.as_ref().unwrap().master_replid);
-    write_buffer.push_str("0\r\n");
+    write_buffer.push_str(&format!(
+        "+FULLRESYNC {} 0\r\n",
+        &server.master_repl.as_ref().unwrap().master_replid
+    ));
 }
