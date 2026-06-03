517e2e74a0dcb634220726eb60c359c6d9753eab
Dylan <carringtonfair.dylan@gmail.com>
2026-06-03 00:55:06 -0400

    bumping up buffer on the reads

--- src/main.rs
+++ src/main.rs
@@ -199,9 +199,8 @@ fn send_error(stream: &mut TcpStream, error: &str) {
 }
 
 fn read_response(stream: &mut TcpStream) -> Result<String> {
-    let mut buffer = [0; 128];
+    let mut buffer = [0; 256];
     let n = stream.read(&mut buffer[..])?;
-    println!("{}", buffer[n]);
     let stream_buf = String::from_utf8((buffer[..n]).to_vec())
         .with_context(|| format!("n is: {} | Buffer is: {:?}", n, buffer))?;
 
