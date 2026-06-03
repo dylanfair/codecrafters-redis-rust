8864280b4a40699441567753b374259f18f32421
Dylan <carringtonfair.dylan@gmail.com>
2026-06-03 00:40:17 -0400

    ok trying this now

--- src/commands/psync.rs
+++ src/commands/psync.rs
@@ -1,6 +1,8 @@
 use std::io::Write;
 use std::{net::TcpStream, sync::Arc};
 
+use nom::ExtendInto;
+
 use crate::send_response;
 use crate::{protocol::parsing::RedisProtocol, server::server::RedisServer};
 
@@ -30,16 +32,19 @@ pub fn handle_psync(
 
         // Open a new connection and send?
         let stream_addr = stream.local_addr().unwrap().to_string();
+        println!("{}", stream_addr);
         let mut connection = TcpStream::connect(stream_addr)
             .expect("Failed to open a connection to send the empty RBD file");
 
         // send RDB transfer
-        let bytes = hex::decode("524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2").expect("Failed to decode empty rdb hex");
-        write_buffer.push_str(&format!("${}\r\n", bytes.len()));
+        let rdb_bytes = hex::decode("524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2").expect("Failed to decode empty rdb hex");
+        let rdb_length = format!("${}\r\n", rdb_bytes.len());
+        let mut bytes = rdb_length.as_bytes().to_vec();
+        rdb_bytes.extend_into(&mut bytes);
 
-        if let Err(e) = connection.write(write_buffer.as_bytes()) {
-            eprintln!("Failed to write response: {}", e);
-        }
+        // if let Err(e) = connection.write(write_buffer.as_bytes()) {
+        //     eprintln!("Failed to write response: {}", e);
+        // }
         if let Err(e) = connection.write_all(&bytes[..]) {
             eprintln!("Failed to write response: {}", e);
         }
