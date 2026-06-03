f388b426dc40b107d4fb7935e16f3cf1ba478fb5
Dylan <carringtonfair.dylan@gmail.com>
2026-06-02 23:44:23 -0400

    forgot to clear write_buffer after initial send

--- src/commands/psync.rs
+++ src/commands/psync.rs
@@ -26,6 +26,7 @@ pub fn handle_psync(
             &server.master_repl.as_ref().unwrap().master_replid
         ));
         send_response(stream, write_buffer);
+        write_buffer.clear();
 
         // send RDB transfer
         let bytes = hex::decode("524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2").expect("Failed to decode empty rdb hex");
