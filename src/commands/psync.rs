74c0b8372a9c9ee2d72d95ce43c091a44e18b570
Dylan <carringtonfair.dylan@gmail.com>
2026-06-03 00:09:29 -0400

    seeing if opening a new connection and sending file works

--- src/commands/psync.rs
+++ src/commands/psync.rs
@@ -28,17 +28,22 @@ pub fn handle_psync(
         send_response(stream, write_buffer);
         write_buffer.clear();
 
+        // Open a new connection and send?
+        let stream_addr = stream.local_addr().unwrap().to_string();
+        let mut connection = TcpStream::connect(stream_addr)
+            .expect("Failed to open a connection to send the empty RBD file");
+
         // send RDB transfer
-        // let bytes = hex::decode("524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2").expect("Failed to decode empty rdb hex");
-        // write_buffer.push_str(&format!("${}\r\n", bytes.len()));
-        // if let Err(e) = stream.write_all(write_buffer.as_bytes()) {
-        //     eprintln!("Failed to write response: {}", e);
-        // }
-        // if let Err(e) = stream.write_all(&bytes) {
-        //     eprintln!("Failed to write response: {}", e);
-        // }
-        // if let Err(e) = stream.flush() {
-        //     eprintln!("Failed to flush stream: {}", e);
-        // }
+        let bytes = hex::decode("524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2").expect("Failed to decode empty rdb hex");
+        write_buffer.push_str(&format!("${}\r\n", bytes.len()));
+        if let Err(e) = connection.write_all(write_buffer.as_bytes()) {
+            eprintln!("Failed to write response: {}", e);
+        }
+        if let Err(e) = connection.write_all(&bytes) {
+            eprintln!("Failed to write response: {}", e);
+        }
+        if let Err(e) = connection.flush() {
+            eprintln!("Failed to flush stream: {}", e);
+        }
     }
 }
